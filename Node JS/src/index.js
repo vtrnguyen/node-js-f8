78a11c9128c32b33c6cf3ec70d0345737fa6bdf5
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2023-09-28 22:56:08 +0700

    node-js-f8

--- Node JS/src/index.js
+++ Node JS/src/index.js
@@ -5,6 +5,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// handle static file
+app.use(express.static(path.join(__dirname, '/public')));
+
 // http logger
 app.use(morgan('combined'));
 
