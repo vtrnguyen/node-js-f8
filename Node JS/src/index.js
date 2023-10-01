85319c1a8c90b6e26ed2b5e543c3c370b028400a
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2023-10-01 21:32:59 +0700

    node-js-f8

--- Node JS/src/index.js
+++ Node JS/src/index.js
@@ -2,14 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const path = require('path');
+const { log } = require('console');
 const app = express();
 const port = 3000;
 
 // handle static file
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // http logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // template engine
 app.engine('hbs', handlebars({
@@ -24,7 +30,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+    console.log(req.query);
     res.render('news');
+})
+
+app.get('/search', (req, res) => {
+    res.render('search');
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.render('search');
 })
 
 app.listen(port, () => {
