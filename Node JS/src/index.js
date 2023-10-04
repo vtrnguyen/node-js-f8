189d7a9127d1673e4633c277aaa61934d1323a0b
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2023-10-05 00:16:00 +0700

    node-js-f8

--- Node JS/src/index.js
+++ Node JS/src/index.js
@@ -28,12 +28,12 @@ app.engine(
         extname: '.hbs',
     }),
 );
-          app.set('view engine', 'hbs');
-              app.set('views', path.join(__dirname, 'resources/views'));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Route init
-route(          app);
+route(app);
 
-                app.listen(port, () => {
-                    console.log(`Example app listening at http://localhost:${port}`);
-                });
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
