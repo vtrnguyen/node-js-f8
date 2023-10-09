b8d160dc4948ed35af4712a2984aa64a5a58bda9
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2023-10-09 16:20:02 +0700

    node-js-f8

---
+++ Node JS/src/app/controllers/CourseController.js
@@ -0,0 +1,31 @@
+const Course = require('../models/Course')
+const { mongooseToObject } = require('../../util/mongoose');
+
+class CourseController {
+    // [GET] /courses/:slug
+    show(req, res, next) {
+        console.log(req.params.slug);
+        Course.findOne({ slug: req.params.slug })
+            .then((course) => res.render('courses/show', { course: mongooseToObject(course) }))
+            .catch(next);
+    }
+
+    // [GET] /courses/create
+    create(req, res, next) {
+        res.render('courses/create');
+    }
+
+    // [POST] /courses/store
+    store(req, res, next) {
+        const formData = req.body;
+        formData.image = `https://img.youtube.com/vi/${formData.videoId}/mqdefault.jpg`;
+        const course = new Course(formData);
+        course.save()
+            .then(() => res.redirect('/'))
+            .catch(error => {
+
+            });
+    }
+}
+
+module.exports = new CourseController();
